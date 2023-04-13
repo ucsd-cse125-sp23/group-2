eb71705cb96b5184566d0df6db22258a661e51c0
William Hu <67835823+WilliamH20@users.noreply.github.com>
2023-04-13 12:24:46 -0700

    rudimentary graphics window

--- Game/Networking/ClientGame.h
+++ Game/Networking/ClientGame.h
@@ -2,12 +2,22 @@
 #include <winsock2.h>
 #include <Windows.h>
 #include "ClientNetwork.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 class ClientGame
 {
 
 public:
 
     ServertoClientData incomingData;
+    GLFWwindow* window;
+
     ClientGame();
     ~ClientGame(void);
 
