e5d321bc5ca91ee9da410eb2c62b00d93580b9b5
GrayFennec <p9lin@ucsd.edu>
2023-05-25 12:52:55 -0700

    Basic loading and playing of sounds

--- Game/src/audio/AudioManager.cpp
+++ Game/src/audio/AudioManager.cpp
@@ -1,20 +1,35 @@
 #include "AudioManager.h"
 #include <string.h>
 AudioManager::AudioManager() {
-    FMOD_RESULT result;
-    FMOD::System* system = nullptr;
+    audioSystem = nullptr;
 
-    result = FMOD::System_Create(&system);      // Create the main system object.
-    if (result != FMOD_OK)
-    {
-        printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
-        exit(-1);
-    }
+    AudioManager::errorCheck(FMOD::System_Create(&audioSystem));      // Create the main system object.
 
-    result = system->init(512, FMOD_INIT_NORMAL, 0);    // Initialize FMOD.
-    if (result != FMOD_OK)
-    {
+    AudioManager::errorCheck(audioSystem->init(512, FMOD_INIT_NORMAL, 0));    // Initialize FMOD.
+
+    AudioManager::loadSound("../assets/sounds/sports.mp3", "bgm");
+    AudioManager::loadSound("../assets/sounds/pew.mp3", "laser");
+    AudioManager::loadSound("../assets/sounds/oof.mp3", "damage");
+    AudioManager::loadSound("../assets/sounds/sentry.mp3", "tower");
+    AudioManager::playSound("bgm");
+}
+
+void AudioManager::loadSound(const char* path, std::string name) {
+    FMOD::Sound* newSound = nullptr;
+    AudioManager::errorCheck(audioSystem->createSound(path, FMOD_DEFAULT, nullptr, &newSound));
+    soundMap.insert(std::pair<std::string, FMOD::Sound*>(name, newSound));
+}
+
+void AudioManager::playSound(std::string name) {
+    FMOD::Channel* newChannel = nullptr;
+    AudioManager::errorCheck(audioSystem->playSound(soundMap.find(name)->second, nullptr, false, &newChannel));
+}
+
+int AudioManager::errorCheck(FMOD_RESULT result) {
+    if (result != FMOD_OK) {
         printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
-        exit(-1);
+        return 1;
     }
+    //printf("FMOD workin!");
+    return 0;
 }
--- Game/src/audio/AudioManager.h
+++ Game/src/audio/AudioManager.h
@@ -3,9 +3,15 @@
 #include <fmod/fmod_common.h>
 #include <fmod/fmod_errors.h>
 #include <iostream>
+#include <unordered_map>
 
 class AudioManager {
 private:
+	FMOD::System* audioSystem;
+	void loadSound(const char* path, std::string name);
+	void playSound(std::string name);
+	int errorCheck(FMOD_RESULT result);
+	std::unordered_map<std::string, FMOD::Sound *> soundMap;
 public:
 	AudioManager();
 };
