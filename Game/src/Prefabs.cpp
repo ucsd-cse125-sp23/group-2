24aed149243ebfc316e3919304e173b962ed32f5
WanderingVagrant <s2bhalla@ucsd.edu>
2023-06-03 17:09:21 -0700

    more balance passes

--- Game/src/Prefabs.cpp
+++ Game/src/Prefabs.cpp
@@ -48,9 +48,9 @@ std::list<Entity> createProjectileSpread5() {
         GameData::colliders[e].AABB =  glm::vec3(.25, .25, .25);
         GameData::models[e].modelID = MODEL_ID_PROJECTILE;
         GameData::models[e].asciiRep = 'J';
-        GameData::coldmg[e].damage = 20.0f;
+        GameData::coldmg[e].damage = 50.0f;
         GameData::lifespans[e] = 1;
-        GameData::spawnrates[e] = PROJ_SPAWN_RATE*2;
+        GameData::spawnrates[e] = PROJ_SPAWN_RATE*1.5;
         GameData::colliders[e].colteam = CollisionLayer::WorldObj;
         GameData::colliders[e].colwith = CollisionLayer::WorldObj + CollisionLayer::StaticObj + CollisionLayer::Boss;
         GameData::hostilities[e].team = Teams::Projectile;
@@ -81,7 +81,7 @@ std::list<Entity> createProjectilePierce() {
     GameData::colliders[e].AABB = glm::vec3(1, 1, 1);
     GameData::models[e].modelID = MODEL_ID_PROJECTILE;
     GameData::models[e].asciiRep = 'J';
-    GameData::coldmg[e].damage = 50.0f;
+    GameData::coldmg[e].damage = 10.0f;
     GameData::lifespans[e] = 5;
     GameData::spawnrates[e] = PROJ_SPAWN_RATE * 4;
     GameData::colliders[e].colteam = CollisionLayer::WorldObj;
